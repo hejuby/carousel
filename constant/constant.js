315a65f2f217e07198ebb9903e11aecf2b7dddfa
Heejun Byeon <hejuby@gmail.com>
2024-02-15 00:30:21 +0900

    fix: change boxicons image to font

--- constant/constant.js
+++ constant/constant.js
@@ -2,16 +2,16 @@ export const TEAM_VALUES = [
   {
     VALUE_NAME: 'Passion',
     VALUE_TEXT: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-    ICON_URL: 'bxs-hot'
+    ICON_URL: '<i class="bx bxs-hot" ></i>'
   },
   {
     VALUE_NAME: 'Teamwork',
     VALUE_TEXT: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-    ICON_URL: 'bxs-network-chart'
+    ICON_URL: '<i class="bx bxs-network-chart" ></i>'
   },
   {
     VALUE_NAME: 'Continous learning',
     VALUE_TEXT: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-    ICON_URL: 'bx-book'
+    ICON_URL: '<i class="bx bx-book" ></i>'
   }
 ];
