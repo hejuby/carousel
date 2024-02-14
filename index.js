bce85b88f767881d78fa3ac607bf38e1fb2237a0
Heejun Byeon <hejuby@gmail.com>
2024-02-15 00:32:13 +0900

    add: press arrows and render different values

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 import { TEAM_VALUES } from './constant/constant.js';
 
-const valueContainer = document.querySelector('.value-container');
-const valueIcon = valueContainer.querySelector('.value-icon');
-const valueTitle = valueContainer.querySelector('.value-title');
-const valueText = valueContainer.querySelector('.value-text');
+const valueIcon = document.getElementById('value-icon-container');
+const valueTitle = document.getElementById('value-title');
+const valueText = document.getElementById('value-text');
+
+const btnValuePrev = document.querySelector('.value-prev');
+const btnValueNext = document.querySelector('.value-next');
+
+let valueIndex = 0;
 
 const renderValue = (index) => {
-  if (valueIcon.classList.length === 3) {
-    valueIcon.classList.pop();
-  }
-  valueIcon.classList.add(TEAM_VALUES[index].ICON_URL);
+  valueIcon.innerHTML = TEAM_VALUES[index].ICON_URL;
   valueTitle.innerText = TEAM_VALUES[index].VALUE_NAME;
   valueText.innerText = TEAM_VALUES[index].VALUE_TEXT;
+  if (index === 0) {
+    btnValuePrev.classList.add('hidden');
+  }
+  if (index === TEAM_VALUES.length-1) {
+    btnValueNext.classList.add('hidden');
+  }
+  if (index > 0 && index < TEAM_VALUES.length-1) {
+    btnValuePrev.classList.remove('hidden');
+    btnValueNext.classList.remove('hidden');
+  }
 };
 
-renderValue(0);
+const pressValuePrev = () => {
+  console.log('prev pressed');
+  valueIndex -= 1;
+  renderValue(valueIndex);
+};
+
+const pressValueNext = () => {
+  console.log('next pressed');
+  valueIndex += 1;
+  renderValue(valueIndex);
+};
+
+renderValue(valueIndex);
+btnValuePrev.addEventListener('click', pressValuePrev);
+btnValueNext.addEventListener('click', pressValueNext);
