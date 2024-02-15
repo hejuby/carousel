882b6549285952f965ba636ec3ba6d5723643fef
Heejun Byeon <hejuby@gmail.com>
2024-02-15 19:53:04 +0900

    feat: add value image feature to the slider

--- constant/constant.js
+++ constant/constant.js
@@ -2,16 +2,22 @@ export const TEAM_VALUES = [
   {
     VALUE_NAME: 'Passion',
     VALUE_TEXT: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-    ICON_URL: '<i class="bx bxs-hot" ></i>'
+    ICON_URL: '<i class="bx bxs-hot" ></i>',
+    IMAGE_URL: '/images/passion.jpg',
+    ATTRIBUTION_URL: '<a href="https://www.vecteezy.com/vector-art/251905-passion-vector">Passion Vector Vectors by Vecteezy</a>'
   },
   {
     VALUE_NAME: 'Teamwork',
     VALUE_TEXT: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-    ICON_URL: '<i class="bx bxs-network-chart" ></i>'
+    ICON_URL: '<i class="bx bxs-network-chart" ></i>',
+    IMAGE_URL: '/images/teamwork.jpg',
+    ATTRIBUTION_URL: '<a href="https://www.vecteezy.com/vector-art/348802-school-teamwork-vector">School Teamwork Vector Vectors by Vecteezy</a>'
   },
   {
     VALUE_NAME: 'Continous learning',
     VALUE_TEXT: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-    ICON_URL: '<i class="bx bx-book" ></i>'
+    ICON_URL: '<i class="bx bx-book" ></i>',
+    IMAGE_URL: '/images/learning.jpg',
+    ATTRIBUTION_URL: '<a href="https://www.vecteezy.com/vector-art/122672-free-teacher-vector">Free Teacher Vector Vectors by Vecteezy</a>'
   }
 ];
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { TEAM_VALUES } from './constant/constant.js';
 const valueIcon = document.getElementById('value-icon-container');
 const valueTitle = document.getElementById('value-title');
 const valueText = document.getElementById('value-text');
+const valueImg = document.querySelector('.value-image-container');
 
 const btnValuePrev = document.querySelector('.value-prev');
 const btnValueNext = document.querySelector('.value-next');
@@ -13,6 +14,8 @@ const renderValue = (index) => {
   valueIcon.innerHTML = TEAM_VALUES[index].ICON_URL;
   valueTitle.innerText = TEAM_VALUES[index].VALUE_NAME;
   valueText.innerText = TEAM_VALUES[index].VALUE_TEXT;
+  valueImg.style.backgroundImage = `url(${TEAM_VALUES[index].IMAGE_URL}`;
+  valueImg.innerHTML = TEAM_VALUES[index].ATTRIBUTION_URL;
 };
 
 const pressValuePrev = () => {
